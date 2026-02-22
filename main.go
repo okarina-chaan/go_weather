2860376e10568393ba900425f603174b7dcde4b7
おかりな_69B <lcttt.pc@gmail.com>
2026-02-22 23:34:02 +0900

    open meteoのエンドポイントURLを緯度経度を用いて動的に生成するコード

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	TokyoLat = 35.6895
+	TokyoLon = 139.6917
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	latitude := TokyoLat
+	longitude := TokyoLon
+	weatherAPIURL := fmt.Sprintf(
+		"https://api.open-meteo.com/v1/forecast?latitude=%f&longitude=%f&hourly=temperature_2m",
+		latitude, longitude,
+	)
+	fmt.Println(weatherAPIURL)
 }
